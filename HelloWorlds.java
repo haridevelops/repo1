595990fc76b46f66865b5d93995631a9d94132ab
harisudhan <m.harisudhan95@gmail.com>
2017-05-28 15:46:41 +0530

    added one java file

---
+++ HelloWorlds.java
@@ -0,0 +1,9 @@
+public class HelloWorlds {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World hari");
+		System.out.println("Hello, World hari");
+    }
+
+}
